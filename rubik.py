880f48f1f0908c6d1e5884f6d50c271f5f0cbbe8
Yuvir Sharma <yuvir@bbd.co.za>
2021-01-16 15:44:50 +0200

    adding rotations

--- rubik.py
+++ rubik.py
@@ -1,5 +1,7 @@
 import sys
 
+from rotations import *
+
 av = sys.argv
 
 def limit_argv(av):
@@ -93,88 +95,15 @@ def display_scrambled_cube(moves):
 	for move in moves:
 		if (move.upper().startswith('F')):
 			if (len(move) == 1):
-				# front
-				rotated_front[0][0] = front[2][0]
-				rotated_front[0][1] = front[1][0]
-				rotated_front[0][2] = front[0][0]
-
-				rotated_front[1][2] = front[0][1]
-
-				rotated_front[2][2] = front[0][2]
-				rotated_front[2][1] = front[1][2]
-				rotated_front[2][0] = front[2][2]
-
-				rotated_front[1][0] = front[2][1]
-
-				#left
-				rotated_left[0][2] = down[0][0]
-				rotated_left[1][2] = down[0][1]
-				rotated_left[2][2] = down[0][2]
-
-				#up
-				rotated_up[2][0] = left[2][2]
-				rotated_up[2][1] = left[1][2]
-				rotated_up[2][2] = left[0][2]
-
-				#right
-				rotated_right[0][0] = up[2][0]
-				rotated_right[1][0] = up[2][1]
-				rotated_right[2][0] = up[2][2]
-
-				#down
-				rotated_down[0][0] = right[2][0]
-				rotated_down[0][1] = right[1][0]
-				rotated_down[0][2] = right[0][0]
+				rotated_front, front, rotated_left, down, rotated_up, left, rotated_right, up, rotated_down, right = front_cw(rotated_front, front, rotated_left, down, rotated_up, left, rotated_right, up, rotated_down, right)
 
 		if (move.upper().startswith('B')):
 			if (len(move) == 1):
-				# back
-				rotated_back[0][0] = back[2][0]
-				rotated_back[0][1] = back[1][0]
-				rotated_back[0][2] = back[0][0]
-
-				rotated_back[1][2] = back[0][1]
-
-				rotated_back[2][2] = back[0][2]
-				rotated_back[2][1] = back[1][2]
-				rotated_back[2][0] = back[2][2]
-
-				rotated_back[1][0] = back[2][1]
-
-				#right
-				rotated_right[0][2] = down[2][2]
-				rotated_right[1][2] = down[2][1]
-				rotated_right[2][2] = down[2][0]
-
-				#up
-				rotated_up[0][0] = right[2][2]
-				rotated_up[0][1] = right[1][2]
-				rotated_up[0][2] = right[0][2]
-
-				#left
-				rotated_left[0][0] = up[0][2]
-				rotated_left[1][0] = up[0][1]
-				rotated_left[2][0] = up[0][0]
-
-				#down
-				rotated_down[2][2] = left[2][0]
-				rotated_down[2][1] = left[1][0]
-				rotated_down[2][0] = left[0][0]
+				rotated_back, back, rotated_left, down, rotated_up, left, rotated_right, up, rotated_down, right = back_cw(rotated_back, back, rotated_left, down, rotated_up, left, rotated_right, up, rotated_down, right)
 
 		if (move.upper().startswith('L')):
 			if (len(move) == 1):
-				# left
-				rotated_left[0][0] = left[2][0]
-				rotated_left[0][1] = left[1][0]
-				rotated_left[0][2] = left[0][0]
-
-				rotated_left[1][2] = left[0][1]
-
-				rotated_left[2][2] = left[0][2]
-				rotated_left[2][1] = left[1][2]
-				rotated_left[2][0] = left[2][2]
-
-				rotated_left[1][0] = left[2][1]
+				rotated_back, back, rotated_left, down, rotated_up, left, rotated_front, up, rotated_down, front = left_cw(rotated_back, back, rotated_left, down, rotated_up, left, rotated_front, up, rotated_down, front)
 
 		if (move.upper().startswith('R')):
 			if (len(move) == 1):
